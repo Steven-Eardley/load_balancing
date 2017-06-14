0cb74edec7604dca5834e152ec34cc3655d0e9bf
Steven Eardley <steve@eardley.xyz>
2017-06-15 00:00:28 +0100

    Added the slow app to the docker compose for haproxy

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from redis import Redis
-import socket, os, time
+import socket, os, time, random
 
 # Create a connection to redis
 r = Redis(host='redis')
@@ -10,7 +10,7 @@ r.set('count', 0)
 # Serve a page which shows the hostname and increments visit counter
 @app.route('/')
 def index():
-    if bool(os.getenv('BROKEN_APP', False)):
-        time.sleep(30)
+    if bool(os.getenv('SLOW_APP', False)):
+        time.sleep(random.randint(0, 33))
     r.incr('count')
     return "Hello, World! From {0}\nVisit count {1}\n".format(socket.gethostname(), int(r.get("count")))

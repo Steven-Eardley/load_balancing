003bbeed8a677082d50051448246f26fcb1f6942
Steven Eardley <steve@eardley.xyz>
2017-06-14 23:50:59 +0100

    Addition of time delay to simulate a misbehaving app.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from redis import Redis
-import socket
+import socket, os, time
 
 # Create a connection to redis
 r = Redis(host='redis')
@@ -10,5 +10,7 @@ r.set('count', 0)
 # Serve a page which shows the hostname and increments visit counter
 @app.route('/')
 def index():
+    if bool(os.getenv('BROKEN_APP', False)):
+        time.sleep(30)
     r.incr('count')
     return "Hello, World! From {0}\nVisit count {1}\n".format(socket.gethostname(), int(r.get("count")))

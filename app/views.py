71078c9bf64241336509218bdf491d5b3c3f2b41
Steven Eardley <steve@eardley.xyz>
2017-06-19 17:33:52 +0100

    The app now tells you how long it sleeps for, and there's now a testing bash script.

--- app/views.py
+++ app/views.py
@@ -11,6 +11,8 @@ r.set('count', 0)
 @app.route('/')
 def index():
     if bool(os.getenv('SLOW_APP', False)):
-        time.sleep(random.randint(0, 33))
+        rnd = random.randint(0, 33)
+        print('Sleeping for {0} seconds before replying.'.format(rnd))
+        time.sleep(rnd)
     r.incr('count')
     return "Hello, World! From {0}\nVisit count {1}\n".format(socket.gethostname(), int(r.get("count")))
---
+++ test_load_balancer.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+count=0;
+status=0;
+
+while [[ ${count} -lt 100 ]]
+    do
+        ((count=count+1))
+        resp=$(curl -s localhost:8080);
+        respcount=${resp#Hello*count }
+
+        echo ${resp}
+
+        # We should exit if the counts are out of sync
+        if [[ ${count} != ${respcount} ]]
+        then
+            echo "Local and remote sequence counts differ: local $count, remote $respcount";
+            status=1;
+            break;
+        fi
+        sleep .5s
+    done
+
+exit ${status};
